3b271ca5ff5433dace0884c3aa06ec22b88ed048
ebrykim <bryan.kim@ericsson.com>
2018-02-19 16:35:22 -0800

    Swapping Sprites

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Brick : MonoBehaviour {
 
 	public int maxHits;
-
+	public Sprite[] hitSprites;
+	
 	private LevelManager levelManager;
 	private int timesHit;
 
@@ -23,11 +24,19 @@ public class Brick : MonoBehaviour {
 		timesHit++;
 		if (timesHit >= maxHits) {
 			Destroy(gameObject);
+		} else {
+			LoadSprites();
 		}
+	}
+
+	void LoadSprites() {
+		int spriteIndex = timesHit - 1;
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
 	}
 	
 	// TODO Remove this method once we can actually win!
 	void SimulateWin() {
 		levelManager.LoadNextLevel();
 	}
+
 }
